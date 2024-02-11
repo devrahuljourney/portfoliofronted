e929a55b781e047a7ef7a79d92a149b62f063a8e
rahulkumarv269 <rahulkumarv269@gmail.com>
2024-02-11 11:59:06 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App border-orange-600 bg-red-500">
+      <h1 className='  ' >THIS IS APP </h1>
+    </div>
+  );
+}
+
+export default App;
