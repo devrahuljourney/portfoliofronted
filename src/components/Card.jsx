ca9edb319fc208af0a0b5f6840e442b3caf9b8e6
rahulkumarv269 <rahulkumarv269@gmail.com>
2024-02-13 18:47:27 +0530

    change

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 export default function Card(props) {
   return (
+    // page not found
     <div className=' hover:bg-[#385170] bg-[#88a0be] md:w-[350px] w-[200px] h-[150px] md:h-[200px] md:text-[22px] text-[18px] flex flex-col justify-center items-center gap-4 rounded-3xl card ' >
         <p className=' font-bold text-white ' > {props.data.title} </p>
         <p className=' md:text-[20px] text-[16px] text-gray-700 font-bold ' > {props.data.description} </p>
